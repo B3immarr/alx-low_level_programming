fea0e9f036d40550e9a6b698c3cedc1804799b07
B3immarr <brett222wambugu@gmail.com>
2023-03-22 07:20:21 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+/**
+* main - even Fibonacci sequence
+*
+* Return: return 0.
+*/
+int main(void)
+{
+unsigned long i = 0, j = 1, sum;
+float total;
+
+while (1)
+{
+sum = i + j;
+if (sum > 4000000)
+break;
+
+if ((sum % 2) == 0)
+total += sum;
+
+i = j;
+j = sum;
+}
+printf("%.0f\n", total);
+
+return (0);
+}

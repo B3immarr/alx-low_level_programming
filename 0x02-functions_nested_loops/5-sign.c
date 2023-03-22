1607ee7c613f0bdc6e335cd50134b0070aaff711
B3immarr <brett222wambugu@gmail.com>
2023-03-22 06:18:37 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,29 +5,24 @@
 *
 * Return: Always 0.
 */
-int main(void)
+int print_sign(int n)
 {
-int r;
+int test;
 
-r = print_sign(98);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(0);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(0xff);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(-1);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+if (n > 0)
+{
+test = 1;
+_putchar('+');
+}
+else if (n == 0)
+{
+test = 0;
+_putchar('0');
+}
+else
+{
+test = -1;
+_putchar('-');
+}
+return (test);
 }

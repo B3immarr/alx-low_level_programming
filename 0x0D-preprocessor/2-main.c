02186ac59311c96506bee698be94a66bf4447058
B3immarr <brett222wambugu@gmail.com>
2023-04-17 12:53:05 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints name of file program was compiled from, then a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("%s/n", _FILE_);
+return (0);
+}
